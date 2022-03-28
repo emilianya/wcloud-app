f4a0905a546157f81c80ff0c3790f471f9b49f41
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-28 12:35:14 +0300

    enter token to get name of first file

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import React, {useState} from 'react';
+
+var token = "";
+const TokenInput = () => {
+	const [firstFile, setFirstFile] = useState('');
+	let requestFiles = (e) => {
+		fetch('https://wanderers.cloud/api/files', {
+			headers: {
+				'authentication': `${token}`,
+			}
+		}).then(res => res.json().then(res => {
+			setFirstFile(res[0].originalName);
+		}))
+	}
+	return (
+		<View style={styles.container}>
+			<TextInput style={{height: 40, color: "black"}} placeholder="typey typey" onChangeText={newText => token = newText}/>
+			<Button title="Authorize" onPress={requestFiles}/>
+			<Text>{firstFile}</Text>
+		</View>
+	);
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<TokenInput> </TokenInput>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		color: "black",
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
